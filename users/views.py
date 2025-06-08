5cda8fd092286e8c7158558d127374705493bebd
Magdalena-Tomala <87173247+Magdalena-Tomala@users.noreply.github.com>
2025-06-08 16:12:54 +0200

    fix

--- users/views.py
+++ users/views.py
@@ -1,8 +1,45 @@
 import traceback
-from rest_framework.views import APIView
+from django.contrib.auth import get_user_model
+from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import CustomUser
+from .serializers import RegisterSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 from .serializers import CustomTokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            "user": {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+            },
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }, status=status.HTTP_201_CREATED)
+        
+class UserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+        })
+        import traceback
 
 class CustomLoginView(APIView):
     def post(self, request):
@@ -11,6 +48,8 @@ class CustomLoginView(APIView):
             serializer.is_valid(raise_exception=True)
             return Response(serializer.validated_data, status=status.HTTP_200_OK)
         except Exception as e:
-            print("🔴 Login failed:", str(e))
+            print("Login error:", e)
             traceback.print_exc()
             return Response({"detail": "Internal server error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    
