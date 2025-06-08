2422ee4954883ed98b6d9ba3891587ce535885bd
Magdalena-Tomala <87173247+Magdalena-Tomala@users.noreply.github.com>
2025-06-08 22:49:35 +0200

    xvxv

--- recipe_app/storage_backends.py
+++ recipe_app/storage_backends.py
@@ -1,10 +1,11 @@
-
-
-import os
 from storages.backends.s3boto3 import S3Boto3Storage
+import os
 
 class SupabaseMediaStorage(S3Boto3Storage):
     bucket_name = os.environ.get("SUPABASE_BUCKET_NAME")
+    custom_domain = f"{os.environ.get('SUPABASE_ENDPOINT')}/storage/v1/object/public/{bucket_name}"
+    access_key = "unused"
+    secret_key = os.environ.get("SUPABASE_SECRET_KEY")
     endpoint_url = os.environ.get("SUPABASE_ENDPOINT")
-    default_acl = "public-read"
-    file_overwrite = False
+    querystring_auth = False
+    addressing_style = "path"
